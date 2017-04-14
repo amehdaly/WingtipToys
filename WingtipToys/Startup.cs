05eef9091ad2073ffa47599c95202868b2b0db34
Ashraf Mehdaly <developer@outlook.sa>
2017-04-15 00:19:08 +0200

    DI Later

--- WingtipToys/Startup.cs
+++ WingtipToys/Startup.cs
@@ -4,6 +4,7 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(WingtipToys.Startup))]
 namespace WingtipToys
 {
+    //ToDo :
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
             ConfigureAuth(app);
